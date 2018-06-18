b56ade1907bc54525999d5f2165643bd67d3b605
faisal5170 <faisalmpathan@gmail.com>
2018-06-18 18:31:05 +0530

    Start

---
+++ Code-First-Demo.Repository/CDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_First_Demo.Repository
+{
+    public class CDBContext : DbContext
+    {
+        public CDBContext() : base("StringDBContext")
+        {                
+        }
+    }
+}
